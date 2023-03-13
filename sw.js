13d53660b57b65a63c7584ff71f67008f6b4e3c2
ssh-shashi <comingtoiit@gmail.com>
2023-03-13 18:02:33 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-eeca666928f9ff25a2c3.js"
+    "url": "webpack-runtime-a5795d2184f96cfce7dd.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "18d94e11840cb39e782e6da582925c0b"
+    "revision": "f29aa3fdd82d1b99b89cf68f1b18ea28"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
